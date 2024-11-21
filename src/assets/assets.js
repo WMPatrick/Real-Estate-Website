e548fec9c0682b4c1abb37790076bfdf674be86c
Patrick Mwangi <pwmwangi2@gmail.com>
2024-11-21 20:48:46 +0300

    Initial Commit

---
+++ src/assets/assets.js
@@ -0,0 +1,100 @@
+import logo from './logo.svg'
+import logo_dark from './logo_dark.svg'
+import cross_icon from './cross_icon.svg'
+import menu_icon from './menu_icon.svg'
+import star_icon from './star_icon.svg'
+import left_arrow from './left_arrow.svg'
+import right_arrow from './right_arrow.svg'
+import header_img from './header_img.png'
+import brand_img from './brand_img.png'
+import project_img_1 from './project_img_1.jpg'
+import project_img_2 from './project_img_2.jpg'
+import project_img_3 from './project_img_3.jpg'
+import project_img_4 from './project_img_4.jpg'
+import project_img_5 from './project_img_5.jpg'
+import project_img_6 from './project_img_6.jpg'
+import profile_img_1 from './profile_img_1.png'
+import profile_img_2 from './profile_img_2.png'
+import profile_img_3 from './profile_img_3.png'
+
+export const assets = {
+    logo,
+    logo_dark,
+    cross_icon,
+    menu_icon,
+    star_icon,
+    header_img,
+    brand_img,
+    project_img_1,
+    project_img_2,
+    project_img_3,
+    project_img_4,
+    left_arrow,
+    right_arrow,
+}
+
+export const projectsData = [
+  {
+    title: "Willow Creek",
+    price: "KSh.2,500,000",
+    location: "Nyali",
+    image: project_img_1,
+  },
+  {
+    title: "Cedar Haven",
+    price: "KSh.2,500,000",
+    location: "Kilifi",
+    image: project_img_2,
+  },
+  {
+    title: "Oak Knoll",
+    price: "Ksh.2,500,000",
+    location: "Lamu",
+    image: project_img_3,
+  },
+  {
+    title: "Pine Ridge",
+    price: "Ksh.2,500,000",
+    location: "Diani",
+    image: project_img_4,
+  },
+  {
+    title: "Meadow Brook",
+    price: "Ksh.2,500,000",
+    location: "Membley",
+    image: project_img_5,
+  },
+  {
+    title: "Forest Glen",
+    price: "Ksh.2,500,000",
+    location: "Loresho",
+    image: project_img_6,
+  },
+];
+
+  export const testimonialsData = [
+    {
+      name: "Donald Jackman",
+      title: "Marketing Manager",
+      image: profile_img_1,
+      alt: "Portrait of Donald Jackman",
+      rating: 5,
+      text: "From the very first meeting, they understood my vision and helped me find the perfect property. Their attention to detail and commitment to client satisfaction is unmatched.",
+    },
+    {
+      name: "Richard Nelson",
+      title: "UI/UX Designer",
+      image: profile_img_2,
+      alt: "Portrait of Richard Nelson",
+      rating: 4,
+      text: " Incredibly helpful throughout the entire home-buying process. Their expertise and efficiency made it a stress-free experience.",
+    },
+    {
+      name: "James Washington",
+      title: "Co-Founder",
+      image: profile_img_3,
+      alt: "Portrait of James Washington",
+      rating: 5,
+      text: "They went above and beyond to find us our dream home. They were always available to answer questions and address concerns promptly.",
+    },
+  ];
